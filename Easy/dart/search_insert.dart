20d36717d85823fbf07afe8a12d988fd34212e8a
selawik <121557430+selawik@users.noreply.github.com>
2023-09-19 21:35:44 +0300

    Removed dart package, add new folder structure

---
+++ Easy/dart/search_insert.dart
@@ -0,0 +1,18 @@
+void main() {
+  final input = [1];
+
+  final input2 = [1, 2];
+  final input3 = [1, 2, 3];
+  final input4 = [1, 2, 3, 4];
+  final input5 = [1, 2, 3, 4, 5];
+
+  print(input.length ~/ 2);
+  print(input2.length ~/ 2);
+  print(input3.length ~/ 2);
+  print(input4.length ~/ 2);
+  print(input5.length ~/ 2);
+}
+
+int searchInsert(List<int> nums, int target) {
+  return 1;
+}
